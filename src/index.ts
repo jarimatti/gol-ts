d819c435e569cb4f178eef18e3ab1ef8dfa29d8e
Jarimatti Valkonen <jarimatti@me.com>
2020-06-04 21:51:14 +0100

    Add option to create custom size board.

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,23 @@ import { render } from './gol/render.js'
 
 class Main {
     private canvas: HTMLCanvasElement
-    private running: boolean = true
+    private runButton: HTMLButtonElement
+    private generationElement: HTMLElement
+    private running: boolean = false
     private board: Board
     private generation = 0
     private timer: any
 
     constructor() {
         this.canvas = document.getElementById('canvas') as HTMLCanvasElement
+        this.runButton = document.getElementById('pause') as HTMLButtonElement
+        this.generationElement = document.getElementById('generation') as HTMLElement
         this.board = randomBoard(this.canvas.height, this.canvas.width, 0)
 
-        this.bindPause()
-        this.bindReset()
+        this.bindRun()
+        this.bindNew()
 
-        this.reset()
+        this.newBoard()
 
         this.setTimer()
     }
@@ -24,34 +28,51 @@ class Main {
         this.timer = setInterval(() => this.step(), 25)
     }
 
-    private bindReset() {
-        const button = document.getElementById('reset') as HTMLButtonElement
+    private bindNew() {
+        const button = document.getElementById('new') as HTMLButtonElement
         button.onclick = () => {
-            this.reset()
+            this.newBoard()
         }
     }
 
-    private reset() {
+    private newBoard() {
         this.generation = 0
 
         const input = document.getElementById('probability') as HTMLInputElement
         const p = input.valueAsNumber / 100.0
 
-        this.board = randomBoard(this.canvas.height, this.canvas.width, p)
+        const width = (document.getElementById('width') as HTMLInputElement).valueAsNumber
+        const height = (document.getElementById('height') as HTMLInputElement).valueAsNumber
+
+        console.log("New board (width, height)", width, height)
+
+        this.canvas.width = width
+        this.canvas.height = height
+        this.board = randomBoard(height, width, p)
+
         render(this.canvas, this.board)
     }
 
-    private bindPause() {
-        const button = document.getElementById('pause') as HTMLButtonElement
-        button.onclick = () => {
+    private bindRun() {
+
+        this.setRunButtonTitle()
+
+        this.runButton.onclick = () => {
             this.running = !this.running
             if (this.running) {
                 this.setTimer()
-                button.innerText = "Pause"
             } else {
                 clearInterval(this.timer)
-                button.innerText = "Continue"
             }
+            this.setRunButtonTitle()
+        }
+    }
+
+    private setRunButtonTitle() {
+        if (this.running) {
+            this.runButton.innerText = "Pause"
+        } else {
+            this.runButton.innerText = "Run"
         }
     }
 
@@ -63,7 +84,7 @@ class Main {
             const stepAt = Date.now()
             render(this.canvas, this.board)
             const renderAt = Date.now()
-            document.getElementById('generation')!.innerText = this.generation.toString()
+            this.generationElement.innerText = this.generation.toString()
             console.log("Generation", this.generation, "durations (step/render/total)", stepAt - start, renderAt - stepAt, renderAt - start)
         }
     }
